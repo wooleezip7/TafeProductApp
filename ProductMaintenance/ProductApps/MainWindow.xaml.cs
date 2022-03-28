bf5749b015e024cbdf16bde3a25df88877a2ba9a
wooleezip7 <Boseong.Kim@student.tafesa.edu.au>
2022-03-28 13:37:07 +1030

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -37,6 +37,7 @@ namespace ProductApps
                 
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBlock.Text = (myPayment + 25.0m).ToString();
+                totalChargeAddingWrapTextBlock.Text = (myPayment + 25.0m + 5.0m).ToString();
 
 
             }
